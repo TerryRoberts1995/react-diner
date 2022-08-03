990f11ca7bf70eac898b28f30e53451305d909f0
Terry Roberts <terryjbroberts@yahoo.com>
2022-08-02 19:16:18 -0500

    added styles and animations for blinking light

--- src/components/app.js
+++ src/components/app.js
@@ -1,43 +1,54 @@
-import React, {useState, useEffect} from 'react';
-import Menu from './Menu';
+import React, { useState, useEffect } from "react";
+import Menu from "./Menu";
 
 export default function App() {
-  const [mainCourse, setMainCourse] = useState({price: 0});
-  const [sideOne, setSideOne] = useState({price: 0});
-  const [sideTwo, setSideTwo] = useState({price: 0});
+  const [mainCourse, setMainCourse] = useState({ price: 0 });
+  const [sideOne, setSideOne] = useState({ price: 0 });
+  const [sideTwo, setSideTwo] = useState({ price: 0 });
   const [totalPrice, setTotalPrice] = useState(0);
-  const [choice, setChoice] = useState('');
-  const [comment, setComment] = useState('');
-  const waitressComments = [`That is a great choice! I love the ${choice.title}`, `${choice.title} is really good!`, `The ${choice.title} goes great with a drink!`];
+  const [choice, setChoice] = useState("");
+  const [comment, setComment] = useState("");
+  const waitressComments = [
+    `That is a great choice! I love the ${choice.title}`,
+    `${choice.title} is really good!`,
+    `The ${choice.title} goes great with a drink!`,
+  ];
 
   const selectFoodItem = (FoodItem) => {
-    if(mainCourse.price === 0) {
+    if (mainCourse.price === 0) {
       setMainCourse(FoodItem);
     } else if (sideOne.price === 0) {
       setSideOne(FoodItem);
-    } else if(sideTwo.price === 0) {
+    } else if (sideTwo.price === 0) {
       setSideTwo(FoodItem);
     }
     setChoice(FoodItem);
-  }
+  };
 
-  const calculateTotalPrice = () => Number(mainCourse.price) + Number(sideOne.price) + Number(sideTwo.price);
+  const calculateTotalPrice = () =>
+    Number(mainCourse.price) + Number(sideOne.price) + Number(sideTwo.price);
 
   useEffect(() => {
-    setTotalPrice(calculateTotalPrice())
-    choice !== '' ? setComment(waitressComments[Math.ceil(Math.random() * waitressComments.length)]) : null;
+    setTotalPrice(calculateTotalPrice());
+    choice !== ""
+      ? setComment(
+          waitressComments[Math.ceil(Math.random() * waitressComments.length)]
+        )
+      : null;
   }, [mainCourse, sideOne, sideTwo, comment]);
 
   return (
-    <div className='app'>
-      <h2 className='diner-title'>Bottega Diner</h2>
-      <div className='all-menus'>
-        <Menu type={'breakfast'} selectFoodItem={selectFoodItem}/>
-        <Menu type={'lunch'} selectFoodItem={selectFoodItem}/>
-        <Menu type={'dinner'} selectFoodItem={selectFoodItem}/>
+    <div className="app">
+      <h2 className="diner-title">Bottega Diner</h2>
+      <div className="all-menus">
+        <Menu type={"breakfast"} selectFoodItem={selectFoodItem} />
+        <Menu type={"lunch"} selectFoodItem={selectFoodItem} />
+        <Menu type={"dinner"} selectFoodItem={selectFoodItem} />
       </div>
-      <div className='total-price'>Total: <span>{totalPrice.toFixed(2)}</span></div>
-      <div className='waitress-comment'>Waitress Comment: {comment}</div>
+      <div className="total-price">
+        Total: <span>{totalPrice.toFixed(2)}</span>
+      </div>
+      <div className="waitress-comment">Waitress Comment: {comment}</div>
     </div>
   );
 }
