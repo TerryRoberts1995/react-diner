08ae3284f4b34838371ccd3ae2569bb1b0c9164f
Terry Roberts <terryjbroberts@yahoo.com>
2022-08-02 22:32:54 -0500

    added hover/click styles for selected items. currently bugged and finding a fix

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const FoodItem = (props) => {
-    const {title, price} = props.FoodItem;
+  const { title, price } = props.FoodItem;
+  const [clicked, setClicked] = useState("");
 
-    return (
-        <div className='foodItem-container' onClick={()=> props.selectFoodItem(props.FoodItem)}>
-            <h3 className='food-display'>{title}: <span className='price-tag'>${price}</span></h3>
-        </div>
-    )
-}
+  return (
+    <div
+      className="foodItem-container"
+      onClick={() => {
+        setClicked("clicked");
+        return props.selectFoodItem(props.FoodItem);
+      }}
+    >
+      <h3 className={`food-display ${clicked}`}>
+        {title}: <span className="price-tag">${price}</span>
+      </h3>
+    </div>
+  );
+};
 
 export default FoodItem;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,43 @@
-import React, {useState, useEffect} from 'react';
-import FoodItem from './FoodItem';
+import React, { useState, useEffect } from "react";
+import FoodItem from "./FoodItem";
 
 const Menu = (props) => {
-    const [allFoodItems, setFoodItems] = useState([]);
-    const url = props.type ==='breakfast' ? 'breakfast' : props.type === 'lunch' ? 'lunch' : 'dinner';
-    
-    const getAllFoodItems = () => {
-        fetch(`http://127.0.0.1:5000/food/get/${url}`)
-        .then(res => res.json())
-        .then(res => setFoodItems(res));
-    }
+  const [allFoodItems, setFoodItems] = useState([]);
+  const url =
+    props.type === "breakfast"
+      ? "breakfast"
+      : props.type === "lunch"
+      ? "lunch"
+      : "dinner";
 
-    const renderAllItems = () => {
-        return allFoodItems.map(item => {
-            return <FoodItem menu={props.type} selectFoodItem={props.selectFoodItem} FoodItem={item}/>;
-        });
-    }
+  const getAllFoodItems = () => {
+    fetch(`http://127.0.0.1:5000/food/get/${url}`)
+      .then((res) => res.json())
+      .then((res) => setFoodItems(res));
+  };
 
-    useEffect(() => {
-        getAllFoodItems();
-    }, []);
+  const renderAllItems = () => {
+    return allFoodItems.map((item) => {
+      return (
+        <FoodItem
+          menu={props.type}
+          selectFoodItem={props.selectFoodItem}
+          FoodItem={item}
+        />
+      );
+    });
+  };
 
-    return (
-        <div className='menu-container'>
-            <h1 className='menu-title'>{props.type.toUpperCase()}</h1>
-            {renderAllItems()}
-        </div>
-    )
-}
+  useEffect(() => {
+    getAllFoodItems();
+  }, []);
+
+  return (
+    <div className="menu-container">
+      <h1 className="menu-title">{props.type.toUpperCase()}</h1>
+      {renderAllItems()}
+    </div>
+  );
+};
 
 export default Menu;
